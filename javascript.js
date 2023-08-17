51846d895d244fe61c5122479bac548aa0de4d7a
Giovanni <giomail33@gmail.com>
2023-08-16 23:16:27 -0400

    added manifest json meta deta

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
+const checkIfPlaying = () => {
+    let vid = $('video').get(0);
+
+    vid.addEventListener('loadstart', function(e) {
+        console.log('vid started!');
+        getQuality();
+    });
+}
+checkIfPlaying();
+
 const getQuality = () => {
     let quality = getVideoPlaybackQuality();
     let screenHeight = window.screen.availHeight;
     let screenWidth = window.screen.availWidth;
 
-    if (quality !== screenHeight && quality !== screenWidth) {
-        const html = document.querySelector('.banner');
-        const bannerDiv = document.createElement('h1');
-        const bannerClass = document.classList.add();
+    if (quality !== screenHeight || quality !== screenWidth) {
+       createNotification();
     }
+}
+
+const createNotification = () => {
+    chrome.notifications.create(
+        {
+            title: 'qualityChecker',
+            message: 'video quality too low!',
+            iconUrl: 'assets/32.png',
+            type: 'basic'
+        }
+    )
 }
