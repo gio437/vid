abb9d6072c52af78143eef160d5ad0c5b5b9f629
Giovanni <giomail33@gmail.com>
2023-08-30 10:29:14 -0400

    created youtube notification with logo and description, removed code and files that are not required

--- javascript.js
+++ javascript.js
@@ -1,59 +1,31 @@
-// const checkIfPlaying = () => {
-//     let vid = $('video').get(0);
-
-//     vid.addEventListener('playing', function(e) {
-//         console.log('vid started!');
-//         getQuality();
-//     });
-// }
-// checkIfPlaying();
-
-// chrome.runtime.sendMessage({ time: '1'});
-
-// chrome.runtime.onMessage.addListener(
-//     function (request, sender, sendResponse) {
-//         if (request.time) {
-//             getQuality();
-//         }
-//     }
-// )
-
-// chrome.runtime.onInstalled.addListener(() => {
-//     console.log('hello');
-// });
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log('worked');
         if (request.greeting === "hello") {
             sendResponse({farewell: "goodbye"});
-            // chrome.notifications.create(
-            //     {
-            //         title: 'qualityChecker',
-            //         message: 'video quality too low!',
-            //         // iconUrl: 'assets/32.png',
-            //         type: 'basic',
-            //         silent: false
-            //     }
-            // );
+            getQuality();
         }
     }
 );
 
 const getQuality = () => {
-    // let videoElem = document.querySelector('video');
-    // let quality = videoElem.getVideoPlaybackQuality();
-    // let screenHeight = window.screen.availHeight;
-    // let screenWidth = window.screen.availWidth;
+    const vidHeight = HTMLVideoElement.height;
+    const vidWidth = HTMLVideoElement.width;
+    const screenHeight = window.screen.availHeight;
+    const screenWidth = window.screen.availWidth;
 
-    // if (quality !== screenHeight || quality !== screenWidth) {
-    //    createNotification();
-    // }
+    if (vidHeight !== screenHeight || vidWidth !== screenWidth) {
+       createNotification();
+    }
 }
 
-// const createMessage = () => {
-//     const response = chrome.runtime.sendMessage({greeting: "hello"});
-//     getQuality();
-//     console.log(response);
-// }
-// createMessage();
+const createNotification = () => {
+    chrome.notifications.create(
+        {
+            title: 'Quality Checker',
+            message: 'Video Quality Too Low!',
+            iconUrl: "youtube-play-button-logo.png",
+            type: 'basic',
+            silent: false
+        }
+    );
+}
