1251da23b62cc450152d28b71afe8fd05a9ac3e7
Giovanni <giomail33@gmail.com>
2023-09-03 23:07:17 -0400

    removed bug regarding url chrome tab query being undefined

--- javascript.js
+++ javascript.js
@@ -8,15 +8,16 @@ chrome.runtime.onMessage.addListener(
 );
 
 // work in progess for detecting current stream quality
+// what are you gonna do with the url? how are you gonna get current screen res with just a url? 
 async function getCurrentTab() {
-    let info = { active: true, currentWindow: true, url };
+    let info = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(info); // Use destructuring to get the first tab
     // let url = tab.url; // Access the URL directly from the tab object
     console.log(url);
     console.log(tab);
-    console.log('hello') // is async being triggered
+    console.log('hello'); // is async being triggered
 }
-getCurrentTab();
+// getCurrentTab();
 
 const getQuality = () => {
     // const vidHeight = 1920; // test
@@ -33,7 +34,7 @@ const createNotification = () => {
     chrome.notifications.create(
         {
             title: 'Quality Checker',
-            message: 'Video Quality Too Low!',
+            message: 'Check Video Quality!',
             iconUrl: "youtube-play-button-logo.png",
             type: 'basic',
             silent: false
--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
 (async () => {
     const response = await chrome.runtime.sendMessage({greeting: "hello"});
     console.log(response);
-  })();
+})();
+
+// to work on click
+// chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
+//         chrome.runtime.sendMessage({greeting: "hello"});
+//     });
