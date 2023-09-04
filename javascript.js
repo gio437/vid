335f0e7c615362c2f2bd7099e8ff3b43968e8db1
Giovanni <giomail33@gmail.com>
2023-09-03 22:16:03 -0400

    will currently remind you to check your video quality before watching to make sure it is the same as your video res, further implentation will only notify if vid res matches screen res

--- javascript.js
+++ javascript.js
@@ -7,15 +7,26 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-const getQuality = () => {
-    const vidHeight = HTMLVideoElement.height;
-    const vidWidth = HTMLVideoElement.width;
-    const screenHeight = window.screen.availHeight;
-    const screenWidth = window.screen.availWidth;
+// work in progess for detecting current stream quality
+async function getCurrentTab() {
+    let info = { active: true, currentWindow: true, url };
+    let [tab] = await chrome.tabs.query(info); // Use destructuring to get the first tab
+    // let url = tab.url; // Access the URL directly from the tab object
+    console.log(url);
+    console.log(tab);
+    console.log('hello') // is async being triggered
+}
+getCurrentTab();
 
-    if (vidHeight !== screenHeight || vidWidth !== screenWidth) {
-       createNotification();
-    }
+const getQuality = () => {
+    // const vidHeight = 1920; // test
+    // const vidWidth = 1080;
+    // const screenHeight = window.screen.availHeight;
+    // const screenWidth = window.screen.availWidth;
+
+    // if (vidHeight !== screenHeight || vidWidth !== screenWidth) {
+        createNotification();
+    // }
 }
 
 const createNotification = () => {
