1dcc5d54ef3aba9d53007688cc4d52f2be8c5391
Giovanni <giomail33@gmail.com>
2023-10-01 12:59:22 -0400

    auto opens quality menu

--- javascript.js
+++ javascript.js
@@ -1,34 +1,11 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.greeting === "hello") {
-            sendResponse({farewell: "goodbye"});
-            getQuality();
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.greeting === "hello") {
+                sendResponse({farewell: "goodbye"});
+                // createNotification();
+            }
         }
-    }
-);
-
-// work in progess for detecting current stream quality
-// what are you gonna do with the url? how are you gonna get current screen res with just a url? 
-async function getCurrentTab() {
-    let info = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(info); // Use destructuring to get the first tab
-    // let url = tab.url; // Access the URL directly from the tab object
-    console.log(url);
-    console.log(tab);
-    console.log('hello'); // is async being triggered
-}
-// getCurrentTab();
-
-const getQuality = () => {
-    // const vidHeight = 1920; // test
-    // const vidWidth = 1080;
-    // const screenHeight = window.screen.availHeight;
-    // const screenWidth = window.screen.availWidth;
-
-    // if (vidHeight !== screenHeight || vidWidth !== screenWidth) {
-        createNotification();
-    // }
-}
+    );
 
 const createNotification = () => {
     chrome.notifications.create(
--- popup.js
+++ popup.js
@@ -1,9 +1,28 @@
 (async () => {
     const response = await chrome.runtime.sendMessage({greeting: "hello"});
     console.log(response);
-})();
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    console.log(`Screen Width: ${screenWidth}`);
+    console.log(`Screen Height: ${screenHeight}`);
 
-// to work on click
-// chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
-//         chrome.runtime.sendMessage({greeting: "hello"});
-//     });
+    const gear = document.getElementsByClassName('ytp-button ytp-settings-button ytp-hd-quality-badge')[0];
+    console.log('vid quality function active');
+    gear.click();
+    const qualityBtn = document.querySelectorAll('.ytp-menuitem')[2];
+    qualityBtn.click();
+    const quality = document.getElementsByClassName('ytp-panel ytp-quality-menu').querySelector('.ytp-menuitem-label');
+    console.log(quality);
+    // quality.click();
+    // quality.click();
+    // quality.forEach(item => {
+    //     if (item.innerHTML === screenHeight) {
+    //         quality.click();
+    //         gear.click();
+    //         console.log('found correct quality');
+    //     }
+    //     else {
+    //         console.log('cant find quality');
+    //     }
+    // })
+})();
