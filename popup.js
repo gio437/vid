290723592528730a5697643ab183f9059833c606
Giovanni <giomail33@gmail.com>
2023-10-02 22:21:55 -0400

    will change quality when new vid is clicked

--- popup.js
+++ popup.js
@@ -1,11 +1,30 @@
 (async () => {
-    const gear = document.getElementsByClassName('ytp-button ytp-settings-button')[0];
-    const qualityBtn = document.querySelectorAll('.ytp-menuitem')[2];
-    const quality = document.querySelector('.ytp-swatch-color-white');
-    if (quality !== null) {
-        console.log('vid quality function active');
-        gear.click();
-        qualityBtn.click();
-        quality.click();
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+              if (mutation.addedNodes.length) {
+                  if (mutation.type == 'childList') {
+                    const gear = document.getElementsByClassName('ytp-button ytp-settings-button')[0];
+                    console.log('vid quality function active');
+                    gear.click();
+                    const qualityBtn = document.querySelectorAll('.ytp-menuitem')[3];
+                    qualityBtn.click();
+                    const quality = document.querySelector('.ytp-swatch-color-white');
+                    quality.click();
+                  }
+              }
+        })
+    })
+    function checkNode() {
+        const targetNode = document.querySelector("h1.title.style-scope.ytd-video-primary-info-renderer");
+        if (!targetNode) {
+            window.setTimeout(checkNode, 500);
+            return;
+        }
+        const config = {
+            childList: true,
+            subtree:true
+        }
+        observer.observe(targetNode, config)
     }
+    checkNode();
 })();
